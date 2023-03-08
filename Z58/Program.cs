3bab162d8cce39d28fc65c6362cdc985dd1f1997
mielsine <mayya.lalayan@gmail.com>
2023-03-09 02:17:03 +0300

    add Z58

--- Z58/Program.cs
+++ Z58/Program.cs
@@ -7,25 +7,43 @@
 // 15 18
 
 Console.Clear();
-Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
 
-int[,] positionSmallElement = new int[1, 2];
-positionSmallElement = FindPositionSmallElement(array, positionSmallElement);
-Console.Write($"Позиция элемента: \n");
-WriteArray(positionSmallElement);
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
 
-int[,] arrayWithoutLines = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
-DeleteLines(array, positionSmallElement, arrayWithoutLines);
-Console.WriteLine($"\nПолучившийся массив:");
-WriteArray(arrayWithoutLines);
+int[,] resultMatrix = new int[m,p];
 
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
 
 int InputNumbers(string input)
 {
@@ -45,54 +63,14 @@ void CreateArray(int[,] array)
   }
 }
 
-void WriteArray(int[,] array)
+void WriteArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i, j] + " ");
+      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
-  }
-}
-
-int[,] FindPositionSmallElement(int[,] array, int[,] position)
-{
-  int temp = array[0, 0];
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i, j] <= temp)
-      {
-        position[0, 0] = i;
-        position[0, 1] = j;
-        temp = array[i, j];
-      }
-    }
-  }
-  Console.WriteLine($"\nMинимальный элемент: {temp}");
-  return position;
-}
-
-void DeleteLines(int[,] array, int[,] positionSmallElement, int[,] arrayWithoutLines)
-{
-  int k = 0, l = 0;
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
-      {
-        arrayWithoutLines[k, l] = array[i, j];
-        l++;
-      }
-    }
-    l = 0;
-    if (positionSmallElement[0, 0] != i)
-    {
-      k++;
-    }
   }
 }
